c8780a2d9ae02c95c085f36d3487dace54ba8d9b
afitra <loeby45@gmail.com>
2023-09-10 14:00:23 +0700

    16-Scope

---
+++ src/main/java/com/example/demo_satu/ScopeConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.demo_satu;
+
+import com.example.demo_satu.data.Dam;
+import com.example.demo_satu.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Slf4j
+@Configuration
+public class ScopeConfiguration {
+
+    /*
+        Secara default bean di eksekusi secara sigleton, namaun jika di tambah kan scope bisa di konfigurasi lanjut
+        sigleton    -> (default) di eksekusi sekali saat build, kecuali saat mode lazy
+        prototype   -> di buat baru setiap kali diakses
+        request     -> di buat baru per Http request (web app)
+        session     -> di buat baru per Http session (web app)
+        application -> di buat baru per servletContext (web app)
+        websocket   -> di buat baru per websocket request (web app)
+     */
+
+    @Bean
+    @Scope("prototype")
+    public Foo fooScope() {
+        log.info("Create Scope fooo");
+        return new Foo();
+    }
+
+    @Bean
+    @Scope("singleton")
+    public Dam damScope() {
+        log.info("Create Scope damm");
+        return new Dam();
+    }
+}
---
+++ src/test/java/com/example/demo_satu/ScopeConfigurationTest.java
@@ -0,0 +1,40 @@
+package com.example.demo_satu;
+
+import com.example.demo_satu.data.Dam;
+import com.example.demo_satu.data.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ScopeConfigurationTest {
+
+    private ApplicationContext context;
+
+    @BeforeEach
+    void setup() {
+        context = new AnnotationConfigApplicationContext(ScopeConfiguration.class);
+    }
+
+    @Test
+    void ScopeConfigurationTest() {
+        Foo foo1 = context.getBean("fooScope", Foo.class);
+        Foo foo2 = context.getBean("fooScope", Foo.class);
+        Foo foo3 = context.getBean("fooScope", Foo.class);
+
+        Assertions.assertNotSame(foo1, foo2);
+        Assertions.assertNotSame(foo1, foo3);
+        Assertions.assertNotSame(foo2, foo3);
+
+
+        Dam dam1 = context.getBean("damScope", Dam.class);
+        Dam dam2 = context.getBean("damScope", Dam.class);
+        Dam dam3 = context.getBean("damScope", Dam.class);
+
+        Assertions.assertSame(dam1, dam2);
+        Assertions.assertSame(dam1, dam3);
+        Assertions.assertSame(dam2, dam3);
+    }
+
+}
