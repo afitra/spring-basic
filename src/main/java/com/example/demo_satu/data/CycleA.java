11a73c15afd2a16b3a69bbf095641a21847b132d
afitra <loeby45@gmail.com>
2023-09-10 12:54:19 +0700

    13-Circular-Dependendies

---
+++ src/main/java/com/example/demo_satu/data/CycleA.java
@@ -0,0 +1,7 @@
+package com.example.demo_satu.data;
+
+public class CycleA {
+    public CycleA(CycleB cycleB) {
+        
+    }
+}
---
+++ src/main/java/com/example/demo_satu/data/CycleC.java
@@ -0,0 +1,7 @@
+package com.example.demo_satu.data;
+
+public class CycleC {
+    public CycleC(CycleA cycleA) {
+        
+    }
+}
