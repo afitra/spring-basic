59c82358f4d7c51e4fefb7aee4b92e8695798b97
afitra <loeby45@gmail.com>
2023-09-10 11:17:57 +0700

    message history#4

--- src/test/java/com/example/demo_satu/DemoSatuApplicationTests.java
+++ src/test/java/com/example/demo_satu/DemoSatuApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.demo_satu;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoSatuApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
