c01d5f0ef2b54bdb4dc59e2498455cdb9f96c88d
afitra <loeby45@gmail.com>
2023-10-01 17:28:07 +0700

    SpringBootApplication n SpringAplication

---
+++ src/main/java/com/example/demo_satu/application/FooApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo_satu.application;
+
+import com.example.demo_satu.data.Foo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FooApplication {
+    @Bean
+    public Foo foo() {
+        return new Foo();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Foo.class, args);
+        Foo foo = applicationContext.getBean(Foo.class);
+        System.out.println(foo);
+    }
+}
