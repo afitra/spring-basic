800e85a45d8e07e29ff3ab71ec9535024d7799d1
afitra <loeby45@gmail.com>
2023-10-01 14:52:32 +0700

    ordered bean post processor

--- src/main/java/com/example/demo_satu/aware/IdAware.java
+++ src/main/java/com/example/demo_satu/aware/IdAware.java
@@ -3,4 +3,6 @@ package com.example.demo_satu.aware;
 public interface IdAware {
     void setId(String id);
 
+    String getId();
+
 }
--- src/main/java/com/example/demo_satu/processor/PrefixIdGeneratorBeanPostProcessor.java
+++ src/main/java/com/example/demo_satu/processor/PrefixIdGeneratorBeanPostProcessor.java
@@ -7,23 +7,21 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Slf4j
 @Component
-public class IdGeneratorBeanPostProcessor implements BeanPostProcessor, Ordered {
+public class PrefixIdGeneratorBeanPostProcessor implements BeanPostProcessor, Ordered {
     @Override
     public int getOrder() {
-        return 1;
+        return 2;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        log.info("ID generator processos for bean {}  ", beanName);
+        log.info("Prefix ID generator processos for bean {}  ", beanName);
         if (bean instanceof IdAware) {
-            log.info("set id genrator for bean {} ", beanName);
+            log.info("Prefix set id genrator for bean {} ", beanName);
             IdAware idAware = (IdAware) bean;
-            idAware.setId(UUID.randomUUID().toString());
+            idAware.setId("PRE-" + idAware.getId());
         }
 
         return bean;
